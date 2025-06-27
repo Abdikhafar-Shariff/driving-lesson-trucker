6474b577c307b5c5672b713ea74c6a49d599715a
Abdikhafar-Shariff <kulersabdi@yahoo.se>
2025-06-27 06:01:42 +0200

    first commit

---
+++ drivinglessontrucker/src/main/java/com/abdikhafar/drivinglessontrucker/DrivinglessontruckerApplication.java
@@ -0,0 +1,13 @@
+package com.abdikhafar.drivinglessontrucker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivinglessontruckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivinglessontruckerApplication.class, args);
+	}
+
+}
---
+++ drivinglessontrucker/src/test/java/com/abdikhafar/drivinglessontrucker/DrivinglessontruckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdikhafar.drivinglessontrucker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivinglessontruckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
